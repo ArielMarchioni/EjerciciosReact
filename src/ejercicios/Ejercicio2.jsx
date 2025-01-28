c8a65c4b8222d2768d47ef1631007ffbbb4678b8
Ariel Marchioni <arielmarchioni@hotmail.com>
2025-01-28 18:44:41 -0300

    titulo , descripcion y estilos

--- src/ejercicios/Ejercicio2.jsx
+++ src/ejercicios/Ejercicio2.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react"; 
 
-export const subtitulo = "el titulo  ";
-export const Estoyaca = "Recogida de Datos";
+export const subtitulo = "Conversor de Euros a Dólares 💰";
+export const Estoyaca = "convertí euros a dólares de forma rápida. Solo ingresá la cantidad en euros, presioná el botón y obtené el resultado en dólares al instante. 🚀";
 
 function Ejercicio2(){
     const eurosRef = useRef();
@@ -11,15 +11,21 @@ function Ejercicio2(){
         const eurosValue = eurosRef.current.value;
         const dolares = eurosValue * 2;
 
-        resultadoRef.current.innerHTML = dolares;
+        resultadoRef.current.innerHTML = dolares + "$ dolares";
     }
 
     return(
-        <div>
-            <h1>Euros a dolares </h1>
-            <input type="text" ref={eurosRef}  placeholder="Ingresa Euros"/>
-            <button onClick={calcular}>Enviar</button>
-            <div ref={resultadoRef}></div>
+        <div className="bg-gradient-to-br from-indigo-500 to-purple-600 text-white mx-auto mt-8 w-80 p-6 rounded-xl shadow-xl">
+            <h1 className="mb-3 text-lg font-semibold text-center">Euros a dolares </h1>
+            <div className="flex flex-col gap-4">
+                <input type="number" ref={eurosRef}  placeholder="Ingresa Euros" autoFocus
+                 className="p-3 border border-gray-300 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-indigo-400 placeholder-gray-500" />
+                <button onClick={calcular}
+                  className="bg-pink-500 hover:bg-pink-600 text-white font-semibold p-3 rounded-lg transition duration-200 shadow-md"
+                >Enviar</button>
+                <div ref={resultadoRef} className="mt-2 p-3 text-center text-lg font-medium  text-white rounded-lg min-h-[40px] shadow-sm"></div>
+            </div>
+            
         </div>
     )
 }
