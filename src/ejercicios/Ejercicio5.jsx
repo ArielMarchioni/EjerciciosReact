7088fabb897214e9e89ec9b5442357c29cc43195
Ariel Marchioni <arielmarchioni@hotmail.com>
2025-02-05 17:45:55 -0300

    estilos css

--- src/ejercicios/Ejercicio5.jsx
+++ src/ejercicios/Ejercicio5.jsx
@@ -5,19 +5,23 @@ export const subtitulo = "Use State ";
 export const Estoyaca = "use state para contar clicks en un boton  " ;
 
 function Ejercicio5() {
-
     const [contador, setContador] = useState(0);
-    function calcular(){
-        setContador(contador + 1);
+  
+    function calcular() {
+      setContador(contador + 1);
     }
-
-
-    return(
-        <div><button onClick={calcular} >
-             {contador}</button></div>
-    )
-
-}
-
-export default Ejercicio5;
-
+  
+    return (
+      <div className="flex justify-center items-center m-auto w-80  h-80 bg-gradient-to-r from-green-300 to-blue-400">
+        <button 
+          className="bg-red-500 text-white text-2xl font-bold px-6 py-3 rounded-lg shadow-lg transition-transform transform hover:scale-110 active:scale-95"
+          onClick={calcular}
+        >
+          {contador}
+        </button>
+      </div>
+    );
+  }
+  
+  export default Ejercicio5;
+  
