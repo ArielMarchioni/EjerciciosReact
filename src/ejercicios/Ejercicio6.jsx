2b73330df060f5a95e4376c44d70dc3bdbf2ebbf
Ariel Marchioni <arielmarchioni@hotmail.com>
2025-02-13 11:01:49 -0300

    Estilos ejercicio 6

--- src/ejercicios/Ejercicio6.jsx
+++ src/ejercicios/Ejercicio6.jsx
@@ -17,10 +17,13 @@ function Ejercicio6(){
 }
 
 return(
-  <div>
-    <input ref={euros} type="number" />
-    <button onClick={convertir}>Convertir</button>
-    <p>{dolares}</p>
+  <div className="bg-gradient-to-br from-indigo-500 to-purple-600 text-white mx-auto mt-8 w-80 p-6 rounded-xl shadow-xl">
+    <h1 className="mb-3 text-lg font-semibold text-center">Euros a dolares </h1>
+    <div className="flex flex-col gap-4">
+    <input ref={euros} type="number"className="p-3 border border-gray-300 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-indigo-400 placeholder-gray-500" />
+    <button  className="bg-pink-500 hover:bg-pink-600 text-white font-semibold p-3 rounded-lg transition duration-200 shadow-md" onClick={convertir}>Convertir</button>
+    <p className="mt-2 p-3 text-center text-lg font-medium  text-white rounded-lg min-h-[40px] shadow-sm">{dolares}</p>
+    </div>
   </div>
 )
 } 
